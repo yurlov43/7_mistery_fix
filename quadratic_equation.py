cb8a24ba5c4a2f57a17a4ff0a50bf7cb968a33ce
yurlov43 <yurlov43@yandex.ru>
2017-10-05 18:00:56 +0300

    Ошибка исправлена

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,12 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
         return root1, None
-    else:
+    if discriminant > 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
+    if discriminant < 0:
+        return None, None
