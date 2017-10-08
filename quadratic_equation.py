5d4f864e548116a2162f30de25e970ef3aea802c
yurlov43 <yurlov43@yandex.ru>
2017-10-08 21:15:45 +0300

    убрал код, делающий одно и тоже

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,12 +3,12 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if discriminant == 0:
+    if discriminant >= 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
-        return root1, None
-    if discriminant > 0:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
-        return root1, root2
-    if discriminant < 0:
+        if discriminant == 0:
+            return root1, None
+        else:
+            root2 = (-b + sqrt(discriminant)) / (2 * a)
+            return root1, root2    
+    else:
         return None, None
